ead8ddc94b1f4b4ff4463c30b9e68b6ca82ec9d5
bigmikecreates <bookingsbigmike@gmail.com>
2026-07-05 05:30:01 +0000

    Implemented Fixed Window Rate Limiter in Python

---
+++ prod_eng/rate_limiters/python/fixed_window_counter.py
@@ -0,0 +1,26 @@
+import time
+from collections import defaultdict
+
+class FixedWindowRateLimiter:
+    def __init__(self, max_requests: int, window_seconds: int):
+        self.max_requests = max_requests
+        self.window_seconds = window_seconds
+        self.requests = defaultdict(list)
+
+    def allow_requests(self, client_id: str) -> bool:
+        now = time.time()
+        window_start = now - self.window_seconds
+
+        # Remove expired reqeusts
+        self.requests[client_id] = [
+            timestamp
+            for timestamp in self.requests[client_id]
+            if timestamp > window_start
+        ]
+
+        # Check if client is still within limit
+        if len(self.requests[client_id]) < self.max_requests:
+            self.requests[client_id].append(now)
+            return True
+        
+        return False
