f28d30b46a1f876f412773ddf93984d55c1fdb7c
bigmikecreates <bookingsbigmike@gmail.com>
2026-07-08 13:05:26 +0000

    Implemented MRU Cache in Python

--- prod_eng/caching/mru.py
+++ prod_eng/caching/mru.py
@@ -66,3 +66,28 @@ Common Use Cases
 - Database scan workloads
 - Certain scientific computing applications
 """
+
+from collections import OrderedDict
+
+class MRUCache:
+    def __init__(self, capcity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+
+        self.cache[key] = value
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=True)
+
+    
